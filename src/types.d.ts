b98bfbeb1c5ab08ce087058153972b74a30f2a88
Oscar Sims <oscar@oscarsims.co.nz>
2025-06-12 05:26:15 +0300

    Add types file

---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+declare module 'd3-geo/src/projection/azimuthalEquidistant';
+declare module 'jsts/org/locationtech/jts/operation/buffer/BufferOp';
+declare module 'jsts/org/locationtech/jts/io/GeoJSONReader';
+declare module 'jsts/org/locationtech/jts/io/GeoJSONWriter';
